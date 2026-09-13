2eec7197d920c6e13d3caaa0ccede406d327b2b3
traginodavid57 <traginodavid57@gmail.com>
2026-09-13 15:23:05 -0300

    fix: adicionar import background no ToolsScreen

--- app/src/main/java/org/fynex/manager/ui/screens/tools/ToolsScreen.kt
+++ app/src/main/java/org/fynex/manager/ui/screens/tools/ToolsScreen.kt
@@ -1,6 +1,7 @@
 package org.fynex.manager.ui.screens.tools
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
